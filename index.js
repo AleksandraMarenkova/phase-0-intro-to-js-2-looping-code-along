326a34c0552122e6ebb97b396747dc565a632fd0
Aleksandra <marenkowasasha@icloud.com>
2021-03-26 21:20:22 +0900

    Complite

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function countDown(ten){
+    while(ten>=0){
+    console.log(ten);
+    ten--;
+
+    }
+
+}
+function writeCards(arrayname,event){
+let brisolt= [];
+for (let i =0; i<arrayname.length;i++){
+    brisolt[i]=  `Thank you, ${arrayname[i]}, for the wonderful ${event} gift!`;
+}
+return brisolt;
+}
